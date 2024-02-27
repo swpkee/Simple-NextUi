484e354c42b824b5624a619041173feda467deea
swpkee <aodzaza1130@gmail.com>
2024-02-27 09:36:11 +0700

    Test commit

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { IconSvgProps } from "@/types";
+import {Avatar} from "@nextui-org/react";
 
 export const Logo: React.FC<IconSvgProps> = ({
 	size = 36,
@@ -7,20 +8,9 @@ export const Logo: React.FC<IconSvgProps> = ({
 	height,
 	...props
 }) => (
-	<svg
-		fill="none"
-		height={size || height}
-		viewBox="0 0 32 32"
-		width={size || width}
-		{...props}
-	>
-		<path
-			clipRule="evenodd"
-			d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-			fill="currentColor"
-			fillRule="evenodd"
-		/>
-	</svg>
+	<div className="flex gap-4 pr-2 items-center">
+	<Avatar isBordered color="default" src="/images/DevilEye1.jpeg" />
+  </div>
 );
 
 export const DiscordIcon: React.FC<IconSvgProps> = ({
